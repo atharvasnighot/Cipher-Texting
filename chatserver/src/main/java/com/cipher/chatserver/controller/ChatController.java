52f67910318aeb5dd3a4e5b6a9fce13449071362
atharvasnighot <atharvanighot@gmail.com>
2023-11-09 10:15:46 +0530

    Added Chat Controller and Config files.

---
+++ chatserver/src/main/java/com/cipher/chatserver/controller/ChatController.java
@@ -0,0 +1,8 @@
+package com.cipher.chatserver.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+}
---
+++ chatserver/src/main/java/com/cipher/chatserver/model/Status.java
@@ -0,0 +1,7 @@
+package com.cipher.chatserver.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
